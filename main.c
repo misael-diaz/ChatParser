2da490f165c0293c38dd2b25cccfab9cfa58a6f3
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-05-06 20:11:51 -0400

    forges into a Unix filter
    NOTES
    - instead of reading from a file the user is expected to pipe the
      chat onto the app (a Unix filter)
    - the app leverages mremap to grow the virtual address space to
      transliterate the (input) unicode into (output) ascii

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -18,54 +19,97 @@ struct mapping {
 	uint64_t size_timestamp;
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-	errno = 0;
-	int rc = 0;
-	struct stat statbuf = {};
-	char const *chat = "chat.txt";
-	rc = stat(chat, &statbuf);
-	if (-1 == rc) {
-		fprintf(stderr, "%s%s%s", "error: status of file:", chat, "\n");
-		if (errno) {
-			fprintf(stderr, "%s\n", strerror(errno));
-		}
+	if (argc < 1) {
+		fprintf(stderr, "%s", "surprising command-line error (argc !> 0)\n");
 		_exit(1);
 	}
-	fprintf(stdout, "chat-size: %lu\n", statbuf.st_size);
 
-	int fd = open(chat, O_RDONLY);
-	uint64_t const start_chat = lseek(fd, 0, SEEK_SET);
-	uint64_t const end_chat = lseek(fd, 0, SEEK_END);
-	uint64_t const len_chat = (end_chat - start_chat);
-	if (0 == len_chat) {
-		fprintf(stderr, "%s", "error: empty chat\n");
+	int64_t rc = 0;
+	struct stat st = {};
+	rc = fstat(STDIN_FILENO, &st);
+	if (!S_ISFIFO(st.st_mode)) {
+		fprintf(stderr,
+			"%s %s",
+			argv[0],
+			"expects input to come from a pipe\n");
 		_exit(1);
 	}
-	else if (statbuf.st_size != len_chat) {
-		fprintf(stderr, "%s", "error: unexpected chat size mismatch\n");
-		fprintf(stderr, "error: stat.filesize: %lu seek.filesize: %lu", statbuf.st_size, len_chat);
+
+	errno = 0;
+	rc = lseek(STDIN_FILENO, 0, SEEK_CUR);
+	if (-1 == rc) {
+		if (ESPIPE != errno) {
+			fprintf(stderr,
+				"%s %s",
+				argv[0],
+				"expects input to come from a stream-like pipe (a not seekable pipe)\n");
+			_exit(1);
+		}
+	} else {
+		fprintf(stderr,
+			"%s %s",
+			argv[0],
+			"expects input to come from a stream-like pipe (a not seekable pipe)\n");
 		_exit(1);
 	}
 
 	uint64_t const pagesz = sysconf(_SC_PAGESIZE);
-	uint64_t const len_mmap = (len_chat & 1)
-		? (((len_chat + 0) + (pagesz - 1)) & ~(pagesz - 1))
-		: (((len_chat + 1) + (pagesz - 1)) & ~(pagesz - 1));
-
-	fprintf(stdout, "chat-length (bytes): %lu\n", len_chat);
-	fprintf(stdout, "mmap-size (bytes): %lu\n", len_mmap);
-
-	void *srcbuf = mmap(NULL, len_mmap, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (!srcbuf) {
-		fprintf(stderr, "%s", "error: source chat memory mapping failed\n");
+	uint64_t len_mmap = (pagesz << 1);
+	errno = 0;
+	int fd = -1;
+	int64_t of = 0;
+	void *buff = mmap(NULL, len_mmap, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, fd, of);
+	if (MAP_FAILED == buff) {
 		if (errno) {
 			fprintf(stderr, "%s\n", strerror(errno));
 		}
 		_exit(1);
 	}
+
+	rc = madvise(buff, len_mmap, MADV_WILLNEED);
+	if (-1 == rc) {
+		fprintf(stderr, "%s", "error: buff mmap fast access failed\n");
+		if (errno) {
+			fprintf(stderr, "%s\n", strerror(errno));
+		}
+		_exit(1);
+	}
+
+	int64_t bytes_read = 0;
+	do {
+		errno = 0;
+		rc = read(STDIN_FILENO, buff + bytes_read, pagesz);
+		if (rc > 0) {
+			bytes_read += rc;
+			if ((len_mmap - bytes_read) <= pagesz) {
+				buff = mremap(buff, len_mmap, (len_mmap << 1), MREMAP_MAYMOVE);
+				if (MAP_FAILED == buff) {
+					if (errno) {
+						fprintf(stderr, "%s\n", strerror(errno));
+					}
+					_exit(1);
+				}
+				len_mmap <<= 1;
+			}
+		}
+		else if (-1 == rc) {
+			if (errno) {
+				fprintf(stderr, "%s\n", strerror(errno));
+			}
+			_exit(1);
+		}
+	} while (rc);
+
+	uint64_t len_chat = bytes_read;
+	fprintf(stdout, "chat-length (bytes): %lu\n", len_chat);
+	fprintf(stdout, "mmap-size (bytes): %lu\n", len_mmap);
+
+	errno = 0;
+	void *srcbuf = buff;
 	void *dstbuf = mmap(NULL, len_mmap, PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-	if (!dstbuf) {
+	if (MAP_FAILED == dstbuf) {
 		fprintf(stderr, "%s", "error: destination chat memory mapping failed\n");
 		if (errno) {
 			fprintf(stderr, "%s\n", strerror(errno));
@@ -73,16 +117,8 @@ int main()
 		_exit(1);
 	}
 
-	close(fd);
-	if (-1 == (rc = madvise(srcbuf, len_mmap, MADV_WILLNEED))) {
-		fprintf(stderr, "%s", "error: src mmap fast access failed\n");
-		if (errno) {
-			fprintf(stderr, "%s\n", strerror(errno));
-		}
-		_exit(1);
-	}
-
-	if (-1 == (rc = madvise(dstbuf, len_mmap, MADV_WILLNEED))) {
+	rc = madvise(dstbuf, len_mmap, MADV_WILLNEED);
+	if (-1 == rc) {
 		fprintf(stderr, "%s", "error: dest mmap sequential access failed\n");
 		if (errno) {
 			fprintf(stderr, "%s\n", strerror(errno));
